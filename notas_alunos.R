6c6eeee98d4b0edc8fbf55ce95a67242de7d7077
Vhshugo863 <70487114+Vhshugo863@users.noreply.github.com>
2021-02-16 11:41:06 -0300

    Update notas_alunos.R

--- notas_alunos.R
+++ notas_alunos.R
@@ -1,9 +1,15 @@
+#Limpando a m√©moria
 rm(list = ls())
 
+#Criando um df para testa o codigo
+## Utilizando a fu√ßao c() para criar 100 n√∫meros que correponde a 100 alunos
 Alunos <- c(1:100)
+##Sorteando notas de 0 a 10 para os 100 alunos 
 Notas <- sample(c(0:10), 100, replace = TRUE)
+##Juntando as duas variaveis em um df
 df <- data.frame(Alunos, Notas)
 
-df$SituaÁ„o <- ifelse(df$Notas >=5,"Passou",
+#Utilizando ifelse e criando uma nova coluna no df, a partir do que foi solicitado no problema 
+df$Situacao <- ifelse(df$Notas >=5,"Passou",
                       ifelse(df$Notas >=3 & df$Notas <5,
-                             "RecuperaÁ„o",ifelse(df$Notas <3,"Reprovado", NA)))
+                             "Recupera√ß√£o",ifelse(df$Notas <3,"Reprovado", NA)))
